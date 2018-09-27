17e2d08bbf4d6e89c077bbf3d49f428ef16f3de7
andres maya cadavid <andrescadavid4@hotmail.com>
2018-09-27 10:29:48 -0500

    Configuración LandPage heredando de Lands

--- Lands/Lands/ViewsModels/LandItemViewModel.cs
+++ Lands/Lands/ViewsModels/LandItemViewModel.cs
@@ -1,9 +1,30 @@
 ﻿namespace Lands.ViewsModels
 {
+    using GalaSoft.MvvmLight.Command;
     using Models;
+    using System.Windows.Input;
+    using Xamarin.Forms;
+    using Views;
 
     public class LandItemViewModel : Land
     {
+        #region Commands
+        public ICommand SelectLandCommand
+        {
+            get
+            {
+                return new RelayCommand(SelectLand);
+            }
+        }
 
+        private async void SelectLand()
+        {
+            //invocar la Main viewmodel se usa
+            MainViewModel.GetInstance().Land = new LandViewModel(this);
+            await Application.Current.MainPage.Navigation.PushAsync(new LandPage());
+        }
+        #endregion
+
+        
     }
 }
--- Lands/Lands/ViewsModels/LandViewModel.cs
+++ Lands/Lands/ViewsModels/LandViewModel.cs
@@ -1,6 +1,22 @@
 ﻿namespace Lands.ViewsModels
 {
+    using Models;
+
     public class LandViewModel
     {
+        #region properties
+        public Land Land
+        {
+            get;
+            set;
+        }
+        #endregion
+
+        #region Constructors
+        public LandViewModel(Land land)
+        {
+            this.Land = land;
+        } 
+        #endregion
     }
 }
