23d5613aef8e59e4eed503a56d540b32012bad5a
andres maya cadavid <andrescadavid4@hotmail.com>
2018-09-28 09:07:30 -0500

    Se finaliza desarrollo de la Land Page y los Tabed Page

--- Lands/Lands/ViewsModels/LandViewModel.cs
+++ Lands/Lands/ViewsModels/LandViewModel.cs
@@ -9,7 +9,8 @@
     {
         #region Attributes
         private ObservableCollection<Border> borders;
-
+        private ObservableCollection<Currency> currencies;
+        private ObservableCollection<Language> languages;
         #endregion
         #region properties
         public Land Land
@@ -23,6 +24,18 @@
             get { return this.borders; }
             set { this.SetValue(ref this.borders, value); }
         }
+
+        public ObservableCollection<Currency> Currencies
+        {
+            get { return this.currencies; }
+            set { this.SetValue(ref this.currencies, value); }
+        }
+
+        public ObservableCollection<Language> Languages
+        {
+            get { return this.languages; }
+            set { this.SetValue(ref this.languages, value); }
+        }
         #endregion
 
         #region Constructors
@@ -30,6 +43,8 @@
         {
             this.Land = land;
             this.LoadBorders();
+            this.Currencies = new ObservableCollection<Currency>(this.Land.Currencies);
+            this.Languages = new ObservableCollection<Language>(this.Land.Languages);
         }
         #endregion
 
